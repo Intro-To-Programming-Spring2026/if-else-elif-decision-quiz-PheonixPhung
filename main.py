b30f08cb17fafd32312188d53234228d734bb0fd
PheonixPhung <phung.huynhsaca@gmail.com>
2026-01-29 10:13:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ print("Welcome to the Decision Quiz!")
 print("-----------------------------")
 
 print("What is the fastest speed, in mph, an F1 car has reached?")
+answer = input("Enter your answer: "
 
-
-if answer == "231.4"
+if answer == "231.4":
   print("You're correct! It was set by Valtteri Bottas in a Williams.")
         
-elif answer == "164.47"
+elif answer < "231.4":
   print("You're incorrect. This was Max's fastest average speed for a lap in the Monza Grand Prix!")
 
-else answer = "Sauber" 
+else:
   print("You may or may not be slow! This is NOT a speed.")
 # TODO 4: Print a closing message
 print("Thanks for taking the quiz!")
